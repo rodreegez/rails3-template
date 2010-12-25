5d605ae054b74ccf54f8c12236fbd801c8db3c20
Pete O'Grady <pete@peteogrady.com>
2010-12-25 19:37:19 +0000

    Remove unnecessary files

---
+++ template.rb
@@ -0,0 +1,7 @@
+# Remove unnecessary files
+remove_file "README"
+remove_file "public/index.html"
+remove_file "public/favicon.ico"
+remove_file "public/robots.txt"
+remove_file "public/index.html"
+remove_file "public/images/rails.png"

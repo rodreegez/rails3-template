2e2c0ee1d729471cab36b330ee7f091ddc7be29d
Pete O'Grady <pete@peteogrady.com>
2010-12-25 19:40:24 +0000

    Create a default README

--- template.rb
+++ template.rb
@@ -5,3 +5,8 @@ remove_file "public/favicon.ico"
 remove_file "public/robots.txt"
 remove_file "public/index.html"
 remove_file "public/images/rails.png"
+
+# Create README
+file 'README', <<-README
+#{app_name.humanize}
+README

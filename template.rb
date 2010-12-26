5d278878606784ef7bd26ec3bf4026929c6699ab
Pete O'Grady <pete@peteogrady.com>
2010-12-26 14:33:12 +0000

    Minimal scaffold controller template

--- template.rb
+++ template.rb
@@ -335,12 +335,60 @@ hr.space {background:#fff;color:#fff;visibility:hidden;}
 STYLE
 
 # jQuery
-inside "public/javascripts" do
-  get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "rails.js"
-  get "http://code.jquery.com/jquery-1.4.4.js", "jquery.js"
+inside 'public/javascripts' do
+  get 'https://github.com/rails/jquery-ujs/raw/master/src/rails.js', 'rails.js'
+  get 'http://code.jquery.com/jquery-1.4.4.js', 'jquery.js'
 end
 
-gsub_file "config/application.rb", /config\.action_view\.javascript.*\n/, "config.action_view.javascript_expansions[:defaults] = %w(jquery rails)\n"
+gsub_file 'config/application.rb', /config\.action_view\.javascript.*\n/, "config.action_view.javascript_expansions[:defaults] = %w(jquery rails)\n"
+
+# Scaffold Controller Template
+file 'lib/templates/rails/scaffold_controller/controller.rb', <<-SCAFFOLD_CONTROLLER
+class <%= controller_class_name %>Controller < ApplicationController
+  def index
+    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+  end
+
+  def show
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+  end
+
+  def new
+    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+  end
+
+  def edit
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+  end
+
+  def create
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#\{singular_table_name\}]") %>
+
+    if @<%= orm_instance.save %>
+      redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully created.')
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+
+    if @<%= orm_instance.update_attributes("params[:#\{singular_table_name\}]") %>
+      redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully updated.')
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    @<%= orm_instance.destroy %>
+
+    redirect_to(<%= index_helper %>_url)
+  end
+end
+SCAFFOLD_CONTROLLER
 
 # Bundler
 run 'bundle install'

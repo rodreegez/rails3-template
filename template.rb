9dbfd86bfbf95fcf411b036590ae0ecbc5654ea1
Pete O'Grady <pete@peteogrady.com>
2011-01-25 21:37:42 +0000

    Add default styles

--- template.rb
+++ template.rb
@@ -73,15 +73,33 @@ file 'app/views/layouts/application.html.erb', <<-APPLICATION_LAYOUT
 <html>
   <head>
     <title>#{app_title}</title>
-    <%= stylesheet_link_tag 'screen.css', :media => 'screen, projection' %>
+    <%= stylesheet_link_tag 'screen.css', :media => 'screen' %>
     <%= csrf_meta_tag %>
   </head>
   <body>
-    <h1><%= link_to '#{app_title}', root_path %></h1>
-    <%- flash.each do |name, msg| -%>
-      <%= content_tag :div, msg, :class => "#\{name\}" %>
-    <%- end -%>
-    <%= yield %>
+    <div id="header-wrapper">
+      <div class="container">
+        <div id="header">
+          <h1><%= link_to '#{app_title}', root_path %></h1>
+        </div>
+      </div>
+    </div>
+    <div id="content-wrapper">
+      <div class="container">
+        <div id="content">
+        <%- flash.each do |name, msg| -%>
+          <%= content_tag :div, msg, :class => "#\{name\}" %>
+        <%- end -%>
+          <%= yield %>
+        </div>
+      </div>
+    </div>
+    <div id="footer-wrapper">
+      <div class="container">
+        <div id="footer">
+        </div>
+      </div>
+    </div>
     <%= javascript_include_tag :defaults %>
   </body>
 </html>
@@ -113,6 +131,90 @@ remove_file 'app/stylesheets/print.scss'
 remove_file 'app/stylesheets/ie.scss'
 file 'app/stylesheets/screen.scss', <<-CSS
 @import "compass/reset";
+@import "blueprint/grid";
+@import "blueprint/typography";
+@import "compass/css3";
+@import "compass/utilities";
+
+$header-background-color: #145599;
+$content-background-color: #fff;
+$body-background-color: #eee;
+
+$blueprint-font-family: 'Lucida Grande', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+$font-size: 14px;
+$font-color: #333;
+
+$headings-font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+$headings-color: #333;
+
+$header-title-color: #fff;
+
+body {
+  background-color: $body-background-color;
+  @include blueprint-typography;
+  @include blueprint-typography-body($font-size);
+
+  #\{headings()\} {
+    color: $headings-color;
+    font-family: $headings-font-family;
+    font-weight: bold;
+  }
+
+  h1 {
+    letter-spacing: -2px;
+  }
+
+  h2 {
+    letter-spacing: -1px;
+  }
+}
+
+#header-wrapper {
+  background-color: $header-background-color;
+  @include linear-gradient(color-stops(darken($header-background-color, 10), $header-background-color));
+}
+
+#content-wrapper {
+  background-color: $content-background-color;
+}
+
+#footer-wrapper {
+  @include linear-gradient(color-stops(darken($body-background-color, 10), $body-background-color));
+  clear: both;
+  height: 100px;
+}
+
+.container {
+  @include container;
+}
+
+#header, #content, #footer {
+  @include column(24);
+  @include prepend-top;
+  @include append-bottom;
+}
+
+#header {
+  h1 {
+    @include text-shadow(rgba(#000,.3), 1px, 1px, 4px);
+    margin-bottom: 0;
+    color: $header-title-color;
+    a {
+      @include link-colors($header-title-color, $header-title-color, $header-title-color, $header-title-color);
+      @include unstyled-link;
+    }
+  }
+}
+
+#content {
+  min-height: 300px;
+}
+
+#footer {
+  @include quiet;
+  font-size: $blueprint-font-size - 2px;
+  text-align: center;
+}
 CSS
 
 # Git
